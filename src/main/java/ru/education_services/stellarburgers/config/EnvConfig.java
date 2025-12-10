fa1dd057820f3800f67787b53899e0bbbb16db34
Gleb Itskovich <gleb.kotlyar@yandex.ru>
2025-12-10 17:34:17 +0300

    Написаны тесты на метод получения заказов

--- src/main/java/ru/education_services/stellarburgers/config/EnvConfig.java
+++ src/main/java/ru/education_services/stellarburgers/config/EnvConfig.java
@@ -5,4 +5,7 @@ public class EnvConfig {
     public static final String REGISTER_URL = "api/auth/register";
     public static final String DELETE_OR_PATCH_URL = "api/auth/user";
     public static final String LOGIN_URL = "api/auth/login";
+    public static final String INGREDIENT_URL = "/api/ingredients";
+    public static final String ORDERS_URL = "/api/orders";
+    public static final String ALL_ORDERS_URL = "/api/orders/all";
 }
--- src/main/java/ru/education_services/stellarburgers/steps/Steps.java
+++ src/main/java/ru/education_services/stellarburgers/steps/Steps.java
@@ -3,11 +3,13 @@ package ru.education_services.stellarburgers.steps;
 import io.restassured.response.Response;
 import io.qameta.allure.Step;
 import ru.education_services.stellarburgers.pojo_model.login.LoginRequest;
+import ru.education_services.stellarburgers.pojo_model.orders.OrdersRequest;
 import ru.education_services.stellarburgers.pojo_model.patch.PatchRequest;
 import ru.education_services.stellarburgers.pojo_model.register.RegisterRequest;
 
 
 import static io.restassured.RestAssured.given;
+import static ru.education_services.stellarburgers.config.EnvConfig.*;
 
 
 public class Steps {
@@ -69,6 +71,59 @@ public class Steps {
                 .log().status()
                 .log().body()
                 .extract().response();
+        return response;
+    }
+
+    @Step("Получение ингредиентов")
+    public Response getIngredients(String authToken) {
+        Response response = given().log().uri()
+                .when()
+                .get(INGREDIENT_URL);
+        return response;
+    }
+
+    @Step("Создание заказа")
+    public Response postOrders(OrdersRequest ordersRequest, String urlPath, String authToken) {
+        Response response = given().log().method()
+                .log().uri()
+                .log().body()
+                .header("Content-Type", "application/json")
+                .header("Authorization", authToken)
+                .body(ordersRequest)
+                .when()
+                .post(urlPath)
+                .then()
+                .log().status()
+                .log().body()
+                .extract().response();
+        return response;
+    }
+
+    @Step("Регистрация и логин пользователя без передачи объектов")
+    public Response registerAndLogin() {
+        // Создаем объект класса
+        RegisterRequest registerRequest = RegisterRequest.generateRandomRequest();
+        // Регистрируемся
+        registerUser(registerRequest, REGISTER_URL);
+        // Логинимся
+        LoginRequest loginRequest = new LoginRequest(registerRequest.getEmail(), registerRequest.getPassword());
+        Response response = loginUser(loginRequest, LOGIN_URL);
+        return response;
+    }
+
+    @Step("Получение всех заказов")
+    public Response getAllOrders() {
+        Response response = given().log().uri()
+                .when()
+                .get(ALL_ORDERS_URL);
+        return response;
+    }
+    @Step("Получение списка заказов пользователя")
+    public Response getUserOrder(String authToken){
+        Response response = given().log().uri()
+                .header("Authorization", authToken)
+                .when()
+                .get(ORDERS_URL);
         return response;
     }
 }
